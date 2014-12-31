e108e2850a952c747b64dffe22ed7348725a7b19
mike-drucker <mike.drucker@gmail.com>
2014-12-31 11:26:10 -0600

    exception logging

--- src/com/drucker/flightaware/LocationServer.java
+++ src/com/drucker/flightaware/LocationServer.java
@@ -5,6 +5,8 @@ import org.json.*;
 
 public class LocationServer extends NanoHTTPD
 {
+	private final string TAG = "LocationServer";
+	
 	public LocationServer()
 	{super(8080);}
 
@@ -27,6 +29,7 @@ public class LocationServer extends NanoHTTPD
 		}
 		catch (JSONException e)
 		{
+			Log.d(TAG,"JSONException",e);
 			e.printStackTrace();
 			return new Response(Response.Status.INTERNAL_ERROR, MIME_HTML, String.format("Failed to build data to send, '%1$s'", e.getMessage()));
 		}
